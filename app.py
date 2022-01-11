db392805659bc12346cc383ce2b90ca7d9e3dfff
gl@bulme.at <gl@bulme.at>
2022-01-11 20:27:56 +0100

    add validate on submit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 
 from flask.templating import render_template
 
@@ -10,6 +10,17 @@ app.secret_key = "VerySecretSecretKey"
 @app.route("/", methods=["get","post"])
 def index():
     addItemFormObject = AddItemForm()
+
+    if addItemFormObject.validate_on_submit():
+        #post kam zurück und ist valide
+        print(addItemFormObject.title.data)
+        print(addItemFormObject.description.data)
+        print(addItemFormObject.dueDate.data)
+        print(addItemFormObject.isDone.data)
+        #hier in DB Speichern
+        
+        return redirect("/")
+
     return render_template("index.html", headline="Todo Items", form = addItemFormObject)
 
 app.run(debug=True)
